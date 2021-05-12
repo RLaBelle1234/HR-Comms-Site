33a29afeb18a4c83125894e0d5cf52d6c28e67ad
Richard LaBelle <rlabelle@detempsol.org>
2021-05-12 10:08:28 -0400

    Added DESC News Part

---
+++ desc-news-webpart/src/webparts/descNews/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IDescNewsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'DescNewsWebPartStrings' {
+  const strings: IDescNewsWebPartStrings;
+  export = strings;
+}
